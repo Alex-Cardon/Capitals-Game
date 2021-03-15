77e7612d6671fb76e9a5119ecc63407cabf93460
Dimitri-codePop <dimmartin32@gmail.com>
2021-03-15 16:27:06 +0100

    Leaderboard fait

--- app/controller/adminController.js
+++ app/controller/adminController.js
@@ -1,7 +1,7 @@
 const dataMapper = require('../dataMapper')
 
 exports.addName = ((req, res) => {
-    dataMapper.addName(req.query.name, req.session.score, (error, _) => {
+    dataMapper.addName(req.query.name, req.session.score, (error, result) => {
         if(error) console.log(error);
         else {
             req.session.name = req.query.name;
@@ -9,6 +9,7 @@ exports.addName = ((req, res) => {
          }
     })
 })
+
 exports.addNameForm = (req, res) => {
     res.render('addNameForm', {name: req.query.name});
 }
@@ -17,7 +18,6 @@ exports.leaderboard = ((req, res) => {
     dataMapper.leaderboard((error, names) => {
         if(error) console.log(error);
         else {
-            console.log(req.session.name);
             res.render('leaderBoard', {names})
         }
     })
