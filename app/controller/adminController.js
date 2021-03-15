70cd44e53a071f032f499bf68a878a571c526d10
Dimitri-codePop <dimmartin32@gmail.com>
2021-03-15 15:49:44 +0100

    Leaderboard fait

--- app/controller/adminController.js
+++ app/controller/adminController.js
@@ -1,14 +1,5 @@
 const dataMapper = require('../dataMapper')
 
-<<<<<<< HEAD
-exports.addNameForm = ((req, res) => {
-    dataMapper.addName(req.query.name, req.session.score, (error, name) => {
-        if(error) console.log(error);
-        else {
-            req.session.name = req.query.name;
-            res.render('addName', {name, score: req.session.score})
-        }
-=======
 exports.addName = ((req, res) => {
     dataMapper.addName(req.query.name, req.session.score, (error, _) => {
         if(error) console.log(error);
@@ -16,19 +7,14 @@ exports.addName = ((req, res) => {
             req.session.name = req.query.name;
             res.redirect('/leaderBoard')
          }
->>>>>>> 8dbdeac79766ba16f34695f1431c19c94aa53b13
     })
 })
 exports.addNameForm = (req, res) => {
-    res.render('addNameForm');
+    res.render('addNameForm', {name: req.query.name});
 }
 
 exports.leaderboard = ((req, res) => {
-<<<<<<< HEAD
-    dataMapper.leaderboard(req.session.name, req.session.score, (error, names, scores) => {
-=======
-    dataMapper.leaderboard(req.session.name, req.session.score, (error, names) => {
->>>>>>> 8dbdeac79766ba16f34695f1431c19c94aa53b13
+    dataMapper.leaderboard((error, names) => {
         if(error) console.log(error);
         else {
             console.log(req.session.name);
