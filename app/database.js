70ab5ddfd46b34ac3bc2fa02d36f164f5ad09398
Paulhennin <henninpaul@gmail.com>
2021-03-17 14:11:54 +0100

    :tada: Ne laissez pas MAEVA toucher à ça

--- app/database.js
+++ app/database.js
@@ -1,6 +1,6 @@
 const {Client} = require('pg');
 
-const client = new Client(process.env.PG_URI
+const client = new Client(
 {
     user: "nxicoqaqblguek",
     password: "8d5b7376f4d846e9b56abc4709a64f767bb01b7a37062c467e9a0f5027ae1ae9",
